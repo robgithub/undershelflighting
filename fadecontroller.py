51428264d6ddcacd88137ec889eb208511649589
robgithub <rob.github@jumpstation.co.uk>
2019-04-16 00:00:36 +0100

    added command line options to set config

--- fadecontroller.py
+++ fadecontroller.py
@@ -4,7 +4,8 @@
 #
 # Rob 2019-04-15
 
-import colorsys
+import sys, getopt, pickle, os, colorsys
+from mote import Mote
 
 config_filename = "data_fadecontroller.pickle"
 
@@ -49,6 +50,41 @@ def calculate_colour(colour, brightness, change):
 
 def init_motes(colour):
    print("initialising Motes")
+
+def usage():
+    print("Usage python fadecontroller --config-filename --colour-filename {FILENAME} --brightness-filename {FILENAME} --increase {INCREASE} --decrease {DECREASE} --poll-interval {POLL}")
+    print("INCREASE and DECREASE values are between 0.0 - 1.0")
+    print("{POLL} interval is in seconds")
+    print("No parameters are mandatory")
+
+try:
+  opts, args = getopt.getopt(sys.argv[1:], 'o:c:b:i:d:p:h', ['config-filename=', 'colour-filename=', 'brightness-filename=', 'increase=', 'decrease=', 'poll-interval=', 'help'])
+except getopt.GetoptError:
+  usage()
+  sys.exit(2)
+config = { 'colour_filename':"mote.pickle", 'brightness_filename':"data_fadecontrollerbrightness.pickle", 'increase':1.0, 'decrease':0.1, 'poll_interval':10} 
+for opt, arg in opts:
+  if opt in ('-h', '--help'):
+    usage()
+    sys.exit(2)
+  elif opt in ('-c', '--config-filename'):
+    config_filename = arg
+  elif opt in ('-c', '--colour-filename'):
+    config["colour_filename"] = arg
+  elif opt in ('-b', '--brightness-filename'):
+    config["brightness_filename"] = arg
+  elif opt in ('-i', '--increase'):
+    config["increase"] = arg
+  elif opt in ('-d', '--decrease'):
+    config["decrease"] = arg
+  elif opt in ('-p', '--poll-interval'):
+    config["poll_interval"] = arg
+  else:
+    usage()
+    sys.exit(2)
+print(config_filename)
+print(config)
+sys.exit(0)
 
 print("Fade Controller Starting")
 config = load_config(config_filename)
