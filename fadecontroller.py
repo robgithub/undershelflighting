df84448757b4c5f0ff876e2ef9d052cb5a12179c
robgithub <rob.github@jumpstation.co.uk>
2019-04-17 22:13:07 +0100

    basic mote work, sort of works-ish

--- fadecontroller.py
+++ fadecontroller.py
@@ -7,7 +7,7 @@
 import sys, getopt, pickle, os, colorsys, time
 from mote import Mote
 
-config_filename = "data_fadecontroller.pickle"
+_config_filename = "data_fadecontroller.pickle"
 
 # Pickle loader
 def load_pickle(filename, default):
@@ -39,40 +39,75 @@ def load_colour(filename):
 def load_brightness(filename):
    return load_pickle(filename, 1.0) 
 
-def save_colour(colour, filename):
-   # save updated colour
-   print("save colour")
-   save_pickle(filename, colour)
-
 def save_brightness(brightness, filename):
    # save updated brightness
    print("save brightness")
    save_pickle(filename, brightness)
  
-def ping_service(colour, brightness, config):
+def ping_service(mote, colour, brightness, config):
     print("...starting ping service  CTRL+c to quit")
     try:
         while True:
             print("ping service wakes up")
             # check queue
-            is_ping = False
+            is_ping = True
             if is_ping:
-                colour, brightness = calculate_colour(colour, brightness, config["increase"])
+                colour, brightness = calculate_colour_from_change(colour, brightness, config["increase"])
             else:    
-                colour, brightness = calculate_colour(colour, brightness, config["decrease"])
-            save_colour(colour, config["colour_filename"])
+                colour, brightness = calculate_colour_from_change(colour, brightness, 0.0-config["decrease"])
             save_brightness(brightness, config["brightness_filename"])
+            set_mote_sticks(mote, colour)
             time.sleep(config["poll_interval"])
 
     except KeyboardInterrupt:
         print("terminating")
 
-def calculate_colour(colour, brightness, change):
-   # colour conversion
-   return colour, brightness
+def calculate_colour_from_change(colour, brightness, change):
+    # colour conversion
+    print("current brightness",brightness)
+    brightness = brightness + change
+    if brightness <0:
+        brightness = 0
+    if brightness > 1.0:
+        brightness = 1.0
+    print("new brightness",brightness)
+    # just first stick at the moment
+    h,s,v = colorsys.rgb_to_hsv(colour[0][0], colour[0][1], colour[0][2])
+    colour[0][0], colour[0][1], colour[0][2] = rgb_to_decimal(colorsys.hsv_to_rgb(h, s, brightness))
+    return colour, brightness
 
-def init_motes(colour):
-   print("initialising Motes")
+def rgb_to_decimal(in_queue):
+    out_queue = []
+    for i in range(3):
+        if in_queue[i] <= 0:
+            out_queue.append(0.0)
+        else:
+            out_queue.append(255 * in_queue[i]) 
+    return int(out_queue[0]), int(out_queue[1]), int(out_queue[2]) 
+
+def set_mote_sticks(mote, colour):
+    print("set mote to colour", colour)
+    for st in range(1,4+1):
+        setpixels(st,colour[0][0],colour[0][1],colour[0][2],mote)
+    mote.show()
+
+# set all the pixels to single colour for a given stick 
+def setpixels(s, r, g, b, m):
+    for p in range(16):
+        m.set_pixel(s, p, r, g, b)
+
+def init_motes(colour, brightness):
+    print("initialising Motes")
+    #set
+    mote = Mote() # new instance of the Mote class, will do all the USB init
+    for m in range(1,4+1):
+        mote.configure_channel(m, 16, False) # configure for use the Mote sticks
+    #calculate colour for brightness
+    # just first stick at the moment
+    h,s,v = colorsys.rgb_to_hsv(colour[0][0], colour[0][1], colour[0][2])
+    colour[0][0], colour[0][1], colour[0][2] = rgb_to_decimal(colorsys.hsv_to_rgb(h, s, brightness))
+    set_mote_sticks(mote, colour)
+    return mote
 
 # Merge objects
 # Where any member of obj2 is not None it overwrites the same member of obj1
@@ -113,15 +148,14 @@ for opt, arg in opts:
   else:
     usage()
     sys.exit(2)
-print(config_filename)
-config = merge_objects(load_config(config_filename), config_from_args)
-print(config)
+config = merge_objects(load_config(_config_filename), config_from_args)
+#print(config)
 
-save_pickle(config_filename, config)
+save_pickle(_config_filename, config)
 print("Fade Controller Starting")
 colour, brightness = load_state(config["colour_filename"], config["brightness_filename"])
-print(colour, brightness)
+print("inital colour and brightness",colour, brightness)
 
-init_motes(colour)
+mote_instance = init_motes(colour, brightness)
 
-ping_service(colour, brightness, config)
+ping_service(mote_instance, colour, brightness, config)
